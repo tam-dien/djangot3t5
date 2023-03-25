b5b114f2d15cd7a07e541d0c42c1451107ecfd48
Elias-Evergarden <claryvalentinemorgenstern@gmail.com>
2023-03-25 20:37:36 +0700

    message

--- app1/urls.py
+++ app1/urls.py
@@ -4,4 +4,5 @@ from app1 import views
 urlpatterns = [
     path('', views.index),
     path('hi/', views.hi),
+    path('sanpham/<int:id>', views.sanpham),
 ]
--- app1/views.py
+++ app1/views.py
@@ -7,3 +7,30 @@ def index(request):
 
 def hi(request):
     return HttpResponse("đây là hàm hi ở app 1")
+
+L_sanpham = [
+    {
+        "id":1,
+        "name":"Bánh bò",
+        "price":"10000",
+    },
+    {
+        "id":2,
+        "name":"Bún chả",
+        "price":"35000",
+    },
+    {
+        "id":3,
+        "name":"Bánh mì chảo",
+        "price":"40000",
+    },
+]
+
+def sanpham(request, id):
+    text = "Không có thông tin về sản phẩm"
+    
+    for item in L_sanpham:
+        if id == item["id"]:
+            text = "ID: {a}<br>Tên sản phẩm: {b}<br>Giá: {c}".format(a = item["id"], b = item["name"], c = item["price"])
+
+    return HttpResponse(text)
