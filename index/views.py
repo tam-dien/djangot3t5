5e0366259907619ddb28b0d8d62d597431b8386e
apollo <baotrung06092003@gmail.com>
2023-04-25 14:24:16 +0700

    add delete-product

--- index/views.py
+++ index/views.py
@@ -5,12 +5,20 @@ def list_product(request):
     product = Product.objects.all()
     return render(request, "index/list_product.html", {"product": product})
 
+def product(request, id): 
+    product = Product.objects.get(id = id)
+    return render(request, 'index/product.html', {"product": product})
+
 def edit_product(request, id):
-    product = Product.objects.get(id=id)
-    gr_product = GroupProduct.objects.all()
     if request.method == 'GET':
-        return render(request, 'index/edit_product.html', {"product": product, "gr_product": gr_product })
-    elif request.method == 'POST':
+        try:
+            global product
+            product = Product.objects.get(id=id)
+        except:
+            product = None
+        gr_product = GroupProduct.objects.all()
+        return render(request, 'index/form_product.html', {"product": product, "gr_product": gr_product })
+    if request.method == 'POST':
         name = request.POST.get('name')
         price = request.POST.get('price')
         quantity = request.POST.get('quantity')
@@ -24,4 +32,20 @@ def edit_product(request, id):
         return redirect(f'/product/{id}')
 
 def delete_product(request, id):
+    product = Product.objects.get(id=id)
+    product.delete()
     return redirect('/list_product')
+
+def add_product(request):
+    if request.method == "GET":
+        gr_product = GroupProduct.objects.all()
+        return render(request, 'index/form_product.html', {"gr_product": gr_product})
+    if request.method == "POST":
+        product = Product(
+            name=request.POST['name'],
+            price=request.POST['price'],
+            quantity=request.POST['quantity'],
+            group_id=request.POST['group']
+        )
+        product.save()
+        return redirect('/list_product')
